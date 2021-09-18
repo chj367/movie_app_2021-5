c29edbcdff0c318fcbdb5d582c4ce62621d09275
chj367 <rstaru63@gmail.com>
2021-09-18 12:18:00 +0900

    Perfect Source Commit(210915)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <App />,      // <App /><Potato /> 이렇게 여러개 렌더링은 불가능함.
+                // 따라서 Potato는 App에다가 렌더링하고, App을 여기서 렌더링하면됨.
   document.getElementById('root')  // Id가 root인 곳에다가 렌더링
 );
--- src/potato.js
+++ src/potato.js
@@ -2,9 +2,11 @@
 //     return <h1>I love potato</h1>
 // }
 
+// export default Potato   // 이거 입력해야 다른곳에서도 사용 가능.
 
-function Potato(bar){
-    return <h1>I love {bar.foo}</h1>
-}
 
-export default Potato;   // 이거 입력해야 다른곳에서도 사용 가능.
+// function Potato(bar) {    
+// 	return <h1>I love {bar.foofoo}</h1>  // App.js에서 전달받은 foofoo="you"를 반환함.
+// }
+
+// export default Potato;
