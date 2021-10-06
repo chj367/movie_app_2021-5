928c3a74072110b8284876e6631c2362a86ab7fa
chj367 <rstaru63@gmail.com>
2021-10-06 14:29:48 +0900

    Learning school(211006)
    componentDidMount() 추가 및 setTimeout() 추가

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from "react"
 
 class App extends React.Component {
     state = {
-        isLoading: true  // 여기가 true이니 밑에 Loading이 출력됨!
-                         // false하면 밑에 영화 데이터 출력이 뜸!
+        isLoading: true  
     }
+    
+    componentDidMount(){
+        setTimeout( () => {
+            this.setState({ isLoading: false })
+        }, 6000)    // setTimeout(함수, 초)
+                    // 6000ms(=6초)가 지나면 true에서 false로 바뀜
+    }
+
     render() {
         const { isLoading } = this.state
         return (
