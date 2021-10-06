11ed6f960e7c158bd317f7a6dac69a976238aa78
chj367 <rstaru63@gmail.com>
2021-10-06 14:35:07 +0900

    Learning school(211006)
    데이터 저장하는 movies: [] 추가

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react"
 
 class App extends React.Component {
     state = {
-        isLoading: true  
+        isLoading: true,  
+        movies: []
     }
     
     componentDidMount(){
         setTimeout( () => {
             this.setState({ isLoading: false })
-        }, 6000)    // setTimeout(함수, 초)
-                    // 6000ms(=6초)가 지나면 true에서 false로 바뀜
+        }, 6000)
     }
 
     render() {
