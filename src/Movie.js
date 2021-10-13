0fa68f399a42189d3c7c1fb3267af31247bcbe34
chj367 <rstaru63@gmail.com>
2021-10-13 15:51:41 +0900

    Learning school(211013)
    오타 수정 및 소스코드 구조 변경

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types'
 
-function Movie(id, title, year, summary, poster){
+function Movie({id, title, year, summary, poster}){
     return(
-     <h1>{title}</h1>   
+        <div>
+           <h3>{title}</h3>
+           <h5>{year}</h5>
+           <p>{summary}</p>
+        </div>
     )
 }
 
