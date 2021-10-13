43efb08d8a742e8328b04b9735aad7b15695a405
chj367 <rstaru63@gmail.com>
2021-10-13 16:45:59 +0900

    Learning school(211013)
    poster 내용 수정

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends React.Component {
                                         year = {movie.year}
                                         title = {movie.title}
                                         summary = {movie.summary}
-                                        poster = {movie.poster}
+                                        poster = {movie.medium_cover_image}
                                     />
                                 )
                             }) 
