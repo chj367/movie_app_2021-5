608ad3c11fcebe7a139d3dcebccd713c15d2e323
chj367 <rstaru63@gmail.com>
2021-10-27 15:39:46 +0900

    Learning school(211027)
    About 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css"
 import { HashRouter, Route } from 'react-router-dom'
+import About from './routes/About'
 
 function App() {
     return(
         <HashRouter>
-            <Route />
+            <Route path='/about' component={About} />
         </HashRouter>
     )
 }
