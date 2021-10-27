9786c6bdef62addee1f27d4efa3a72c9b06ca764
chj367 <rstaru63@gmail.com>
2021-10-27 15:49:52 +0900

    Learning School(211027)
    Home 루트 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
 import { HashRouter, Route } from 'react-router-dom'
 import About from './routes/About'
+import Home from './routes/Home'
 
 function App() {
     return(
         <HashRouter>
+            <Route path='/' component={Home} />
             <Route path='/about' component={About} />
         </HashRouter>
     )
